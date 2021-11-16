2de280462e47d8886d4b7696a8f2d4114afd1c46
Domino130 <dominique.roubique@selu.edu>
2021-11-16 14:04:31 -0600

    basic header
    title, tabs, and home page added.
    footer files added

--- 420-web-app/src/App.js
+++ 420-web-app/src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './header';
+import Footer from './footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div style={{color:"rgb(255,199,44)", fontSize: 40}}>Southeastern Louisiana University</div>
+        <div style={{color:"white", fontSize: 20}}>Computer Science Department</div>
+      </div>
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ 420-web-app/src/header.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import {Tabs, Tab, Typography, Box} from '@mui/material';
+import PropTypes from 'prop-types';
+import Home from './pages/home';
+
+ function TabPanel(props) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+      >
+        {value === index && (
+          <Box sx={{ p: 3 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+  
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+  
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      'aria-controls': `simple-tabpanel-${index}`,
+    };
+  }
+
+
+  export default function Header() {
+    const [value, setValue] = React.useState(0);
+  
+    const handleChange = (event, newValue) => {
+      setValue(newValue);
+    };
+  
+    return (
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'white' }}>
+          <Tabs TabIndicatorProps={{style: {background:'white'}}} value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab style={{color:"rgb(255,199,44)", fontSize: 15}} label="Home" {...a11yProps(0)} />
+            <Tab style={{color:"rgb(255,199,44)", fontSize: 15}} label="About Us" {...a11yProps(1)} />
+            <Tab style={{color:"rgb(255,199,44)", fontSize: 15}} label="Admission" {...a11yProps(2)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Home />
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          About Us
+        </TabPanel>
+        <TabPanel value={value} index={2}>
+          Admission
+        </TabPanel>
+      </Box>
+    );
+  }
+  
+ 
